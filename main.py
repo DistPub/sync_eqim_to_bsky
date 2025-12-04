02238f3dc031bf89e413a2a39acbcde4669ee253
Smite Chow <xiaopengyou@live.com>
2025-12-04 23:59:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -84,6 +84,7 @@ def get_data(s, n=1):
 
     data = decrypt_aes_base64(content['msg'])
     data = data['list']
+    data.reverse()
 
     if len(data) == 30:
         return data + get_data(s, n + 1)
